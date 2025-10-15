99f93e8e7153d97e39997033f4275ebf23a967a6
glory-yun <yyk1797@gmail.com>
2025-10-16 00:48:06 +0900

    feat: Controller 기능 구현 - 입력 -> 계산 -> 출력의 흐름으로 기능을 구현했습니다. - Error는 Calculator.js에서 Validate.js를 통해 throw되도록 설계했습니다. - 초기 설계에서는 Controller가 Calculator의 parse와 add를 각각 호출했으나, calculate 메서드로 통합하여 Controller가 계산하지 않도록 변경했습니다.

--- src/Controller.js
+++ src/Controller.js
@@ -1,10 +1,24 @@
+import { enterInput, printResult } from "./Views";
+import Calculator from "./Calculator";
+
 class Controller {
   // Calculator 인스턴스 생성
-  // View에서 사용자에게 문자열 입력받기
-  // Calculator에게 문자열을 넘겨주어 숫자를 분리시킴
-  // Calculator에게 숫자 배열을 넘겨주어 숫자 합을 받음
-  // View에서 사용자에게 결과를 출력
-  // 에러가 발생하면 에러를 처리
+  constructor() {
+    this.calculator = new Calculator();
+  }
+  /*
+  - Controller 실행
+  - 입력 -> 계산 -> 출력 
+  - Error는 Calculator.js에서 문자열을 검사하면서, Validate.js를 통하여 throw됨.
+  */
+  async start() {
+    // 사용자에게 문자열 입력받기
+    const input = await enterInput();
+    // 문자열 계산기에서 계산(숫자 덧셈) 수행
+    const result = this.calculator.calculate(input)
+    // 사용자에게 결과를 출력
+    printResult(result);
+  }
 }
 
 export default Controller;
