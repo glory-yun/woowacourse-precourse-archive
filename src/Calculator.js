ce803475e868d5ec67964f6854c2f7ba17d1fb40
glory-yun <yyk1797@gmail.com>
2025-10-16 16:31:37 +0900

    feat(calculator): Calculator 클래스 구조 설계 및 메서드 정의 - calculate, #findCustomSeperator, #parse, #add 메서드 구조 정의했습니다.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,9 +1,21 @@
 class Calculator {
-  // Controller에서 넘겨받은 입력값 저장
+  //계산기 계산 실행 함수
+  calculate(input) {
+    const separator = this.#findCustomSeperator(input);
+    const parsedNumberArray = this.#parse(input, separator);
+    return this.#add(parsedNumberArray);
+  }
+
   // 커스텀 구분자 찾기
+  #findCustomSeperator(input) {
+  }
+
   // 구분자를 기준으로 문자열 분리
-  // 숫자 배열 반환
-  // 배열의 합 계산 및 반환
+  #parse(input, separator) {
+  }
+  // 배열의 합 계산
+  #add(parsedNumberArray) {
+  }
 }
 
 export default Calculator;
