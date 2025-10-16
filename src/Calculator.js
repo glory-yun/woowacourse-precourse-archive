742a1b3c4e4d496741b15be406dd02e5b996b1ac
glory-yun <yyk1797@gmail.com>
2025-10-16 19:28:22 +0900

    feat: 커스텀 구분자 추출 및 검증 기능 구현 - 입력 문자열에서 //와 n 사이의 커스텀 구분자를 추출했습니다. - Validate.js에서 커스텀 구분자의 형식과 타입을 검증했습니다. - 검증 통과 시 기본 구분자 또는 커스텀 구분자를 포함한 구분자의 정규식을 반환하도록 했습니다.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,16 +1,27 @@
-import { isEmptyInput } from "./utills/Validate.js";
+import { isEmptyInput, validCustomSeparatorFormat, validCustomSeparatorType } from "./utills/Valid.js";
 class Calculator {
   //계산기 계산 실행 함수
   calculate(input) {
     //빈 문자열인지 검사
     if (isEmptyInput(input)) return 0;
-    const separator = this.#findCustomSeperator(input);
+    const separator = this.#findCustomSeparator(input);
     const parsedNumberArray = this.#parse(input, separator);
     return this.#add(parsedNumberArray);
   }
 
   // 커스텀 구분자 찾기
-  #findCustomSeperator(input) {
+  #findCustomSeparator(input) {
+    const defaultSeparator = /,|:/;
+
+    if (!input.startsWith("//")) return defaultSeparator;
+
+    // "\n" 이 존재하는지 확인
+    validCustomSeparatorFormat(input);
+    const customSeparator = input.slice(2, input.lastIndexOf("\\n"));
+    // 커스텀 구분자가 문자인지 검사
+    validCustomSeparatorType(customSeparator);
+
+    return new RegExp(`,|:|${customSeparator}`);
   }
 
   // 구분자를 기준으로 문자열 분리
---
+++ src/utils/Validate.js
@@ -0,0 +1,20 @@
+// 입력받은 문자열이 빈 문자열인지 검사
+function isEmptyInput(input) {
+  return input == "";
+}
+// "//"와 "\n" 사이의 커스텀 구분자가 존재하는지 검사
+function validCustomSeparatorFormat(input) {
+  // "//" 로 시작하지만, "\n" 이 없을 때
+  if (!input.includes("\\n"))
+    throw new Error("[ERROR] 커스텀 구분자를 지정할 수 없습니다.");
+}
+// 커스텀 구분자가 문자인지 아닌지 검사
+function validCustomSeparatorType(customSeparator) {
+  if (/\d/.test(customSeparator))
+    throw new Error("[ERROR] 커스텀 구분자는 문자로 이루어져 있습니다.");
+}
+
+// "," ":"  커스텀 구분자 이외에 다른 문자가 있는지 검사
+// 배열에 음수가 포함되있는지 검사
+
+export { isEmptyInput, validCustomSeparatorFormat, validCustomSeparatorType }
