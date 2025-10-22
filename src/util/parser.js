18c877915f469712d1039e2dec0a2f5f14ec5938
glory-yun <yyk1797@gmail.com>
2025-10-22 23:46:00 +0900

    feat: 빈 자동차 목록 검증 기능 구현
    - 문자열은 있지만, 자동차 이름을 입력하지 않은 경우 예외처리 했습니다.
    - parser.js에 함수 호출하여 빈 목록에 대한 검증을 추가했습니다.

--- src/util/parser.js
+++ src/util/parser.js
@@ -1,4 +1,4 @@
-import { validateCarName } from '../util/validate.js'
+import { validateCarName, validateListIsEmpty } from '../util/validate.js'
 
 export function parseCarName(carInput) {
   //"," 로 구분, 양 옆 공백 제거, 빈 문자열 제거, 
@@ -7,6 +7,8 @@ export function parseCarName(carInput) {
     .map(carName => carName.trim())
     .filter(carName => carName.length > 0);
 
+  //자동차가 있는지 검사
+  validateListIsEmpty(carNameList);
   //자동차 이름이 5자 이상인지 검사
   validateCarName(carNameList);
 
--- src/util/validate.js
+++ src/util/validate.js
@@ -3,3 +3,8 @@ export function validateCarName(carNameList) {
     throw new Error("[ERROR] 자동차 이름은 5글자 이하여야 합니다.")
   }
 }
+export function validateListIsEmpty(carNameList) {
+  if (!carNameList.length) {
+    throw new Error("[ERROR] 자동차는 1개 이상 입력해야 합니다.")
+  }
+}
