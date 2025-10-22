d8ce2f707114cb082aa36aab8d7cf365857748d4
glory-yun <yyk1797@gmail.com>
2025-10-22 21:12:43 +0900

    feat: Racing 모델 클래스 구현
    - Racing 객체를 만들기 전, 객체에 필요한 기능을 먼저 생각한 뒤 테스트 코드에 작성했습니다.
    - race : 각 자동차에 대해 0에서 9 사이의 난수를 생성하여 4 이상일 경우 전진
    - getRacingResult : 현재 모든 자동차의 이름과 위치를 배열로 반환

---
+++ src/model/Racing.js
@@ -0,0 +1,29 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class Racing {
+  #Cars;
+  #raceResult;
+
+  constructor(Cars) {
+    this.#Cars = Cars;
+  }
+
+  race() {
+    this.#Cars.forEach((car) => {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randomNumber >= 4) {
+        car.move();
+      }
+    });
+  }
+
+  getRacingResult() {
+    this.#raceResult = this.#Cars.map(car => (
+      { carName: car.getName(), position: car.getPosition() }
+    ));
+    return this.#raceResult;
+  }
+
+}
+
+export default Racing
