85876272b7f4b7c39f8175571e0b1469ad2297e0
glory-yun <yyk1797@gmail.com>
2025-10-22 23:05:07 +0900

    feat : 자동차 이름 파싱 기능 구현
    - `,`를 기준으로 문자열을 분리하여 자동차 이름 배열 생성을 생성했습니다.
    - 각 이름의 양 끝 공백을 제거하고 빈 문자열은 필터링했습니다.
    - validateCarName을 호출하여 각 이름의 글자수가 5자 이하인지 검증하는 로직을 추가했습니다.

---
+++ src/util/parser.js
@@ -0,0 +1,14 @@
+import { validateCarName } from '../util/validate.js'
+
+export function parseCarName(carInput) {
+  //"," 로 구분, 양 옆 공백 제거, 빈 문자열 제거, 
+  const carNameList = carInput
+    .split(',')
+    .map(carName => carName.trim())
+    .filter(carName => carName.length > 0);
+
+  //자동차 이름이 5자 이상인지 검사
+  validateCarName(carNameList);
+
+  return carNameList
+}
