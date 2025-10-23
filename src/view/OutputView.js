5ba16f38330d0f4d048ba475ddb62bb9fe0da2e4
glory-yun <yyk1797@gmail.com>
2025-10-23 15:25:49 +0900

    feat: 라운드 별 레이싱 결과 출력 기능 구현
    - 각 라운드 별 결과를 입력받아, 자동차마다 얼마나 움직였는지 출력했습니다
    - 줄바꿈을 위하여 printLineSpacing() 를 호출했습니다.

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,3 +3,11 @@ import { Console } from "@woowacourse/mission-utils";
 export function printLineSpacing(message = '') {
   Console.print(message);
 }
+
+export function printCurrentRaceResult(currentRaceResult) {
+  currentRaceResult.forEach(result => {
+    const repeatPositionBar = '-'.repeat(result.position);
+    Console.print(`${result.carName} : ${repeatPositionBar}`);
+  });
+  printLineSpacing();
+}
