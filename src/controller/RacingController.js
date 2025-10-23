8f6a895d3bc2ddad8f288e6c6c28bd4711668d5f
glory-yun <yyk1797@gmail.com>
2025-10-23 16:46:18 +0900

    feat: 레이싱 컨트롤러 구현
    - 1. 사용자 입력을 받아 자동차 이름과 시도 횟수를 받음
    - 2. 입력값을 기반으로 Car 및 Racing 객체를 생성
    - 3. 시도 횟수만큼  race를 하고 각 라운드의 결과를 출력
    - 4. 최종 우승자를 찾아 결과를 출력

---
+++ src/controller/RacingController.js
@@ -0,0 +1,37 @@
+import { enterInput } from "../view/InputView.js";
+import { printCurrentRaceResult, printLineSpacing, printFinalRacingWinner } from "../view/OutputView.js";
+import { parseCarName } from "../util/parser.js"
+import { findRacingWinners } from "../util/findRacingWinners.js";
+import Car from '../model/Car.js';
+import Racing from '../model/Racing.js';
+
+class RacingController {
+  async start() {
+
+    const [carInput, racingCount] = await enterInput();
+
+    //문자열 분리
+    const carNameList = parseCarName(carInput);
+
+    //Car 객체 배열 및 Racing 객체 생성
+    const cars = carNameList.map(car => new Car(car));
+    const racing = new Racing(cars);
+
+    //racingCount만큼 자동차들을 race 후 그 결과 출력하기
+    printLineSpacing('\n실행결과')
+    for (let count = 0; count < racingCount; count++) {
+      racing.race();
+      const currentRaceResult = racing.getRacingResult();
+      printCurrentRaceResult(currentRaceResult);
+    }
+
+    //최종 우승자 찾기
+    const finalResult = racing.getRacingResult();
+    const winnersName = findRacingWinners(finalResult);
+
+    // 최종 우승자 출력
+    printFinalRacingWinner(winnersName);
+  }
+}
+
+export default RacingController;
