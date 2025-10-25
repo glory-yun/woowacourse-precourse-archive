3f27a7bee69d4ba14be3fa8e2b6ef1ac0d699c8a
glory-yun <yyk1797@gmail.com>
2025-10-26 00:15:00 +0900

    test: Car 모델 테스트 코드 수정
    - test.each를 사용하여 move() 테스트를 개선했습니다.
    - 다양한 이동 횟수에 대한 케이스를 검증하도록 수정했습니다.

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -3,19 +3,31 @@ import Car from '../src/model/Car.js';
 
 describe('Car', () => {
 
-  const car = new Car('glory');
-
   test('getPosition() : 자동차의 현재 위치를 반환', () => {
+
+    const car = new Car('glory');
+
     expect(car.getPosition()).toBe(0);
   });
 
-  test('move() : position이 1 증가', () => {
-    car.move();
-    car.move();
-    expect(car.getPosition()).toBe(2);
-  });
+  test.each([
+    [0, 0],
+    [1, 1],
+    [2, 2]
+  ])('move() : %s번 이동시 position이 %s 증가', (moveCount, position) => {
+
+    const car = new Car('glory');
+
+    for (let count = 0; count < moveCount; count++) {
+      car.move();
+    }
+    expect(car.getPosition()).toBe(position);
+  })
 
   test('getName() : 자동차의 이름을 반환', () => {
+
+    const car = new Car('glory');
+
     expect(car.getName()).toBe('glory');
   });
 
