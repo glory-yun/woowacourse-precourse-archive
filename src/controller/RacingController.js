4424ec480dd22a8798724dca55deaaa5c2b1990f
glory-yun <yyk1797@gmail.com>
2025-10-26 18:35:58 +0900

    feat: 컨트롤러에 예외 처리 로직 추가
    - 전체 로직을 try로 감싸 실행 과정에서 발생하는 에러를 감지하게 했습니다.
    - 에러 발생 시 catch에서 printError를 통하여 에러를 출력하도록 했습니다.
    - 사용자에게 메시지를 출력한 후, 다시 에러를 throw하여 프로그램이 비정상 종료되도록 처리했습니다.

--- src/controller/RacingController.js
+++ src/controller/RacingController.js
@@ -2,7 +2,8 @@ import { enterInput } from "../view/InputView.js";
 import {
   printCurrentRaceResult,
   printLineSpacing,
-  printFinalRacingWinner
+  printFinalRacingWinner,
+  printError
 } from "../view/OutputView.js";
 import { PLAY_RESULT } from "../constants/RacingControllerConstants.js";
 import { parseCarName } from "../util/parser.js"
@@ -11,26 +12,31 @@ import RacingGame from '../model/RacingGame.js';
 
 class RacingController {
   async start() {
+    try {
 
-    const [carInput, racingCount] = await enterInput();
+      const [carInput, racingCount] = await enterInput();
 
-    //문자열 분리
-    const carNameList = parseCarName(carInput);
+      //문자열 분리
+      const carNameList = parseCarName(carInput);
 
-    //Car 객체 배열 및 Racing 객체 생성
-    const cars = carNameList.map(car => new Car(car));
-    const racingGame = new RacingGame(cars);
+      //Car 객체 배열 및 Racing 객체 생성
+      const cars = carNameList.map(car => new Car(car));
+      const racingGame = new RacingGame(cars);
 
-    //racingCount만큼 자동차들을 race 후 그 결과 출력하기
-    printLineSpacing(PLAY_RESULT);
-    const allRacingResult = racingGame.play(racingCount);
-    allRacingResult.forEach(result => printCurrentRaceResult(result));
+      //racingCount만큼 자동차들을 race 후 그 결과 출력하기
+      printLineSpacing(PLAY_RESULT);
+      const allRacingResult = racingGame.play(racingCount);
+      allRacingResult.forEach(result => printCurrentRaceResult(result));
 
-    //최종 우승자 찾기
-    const winnersName = racingGame.getWinners();
+      //최종 우승자 찾기
+      const winnersName = racingGame.getWinners();
 
-    // 최종 우승자 출력
-    printFinalRacingWinner(winnersName);
+      // 최종 우승자 출력
+      printFinalRacingWinner(winnersName);
+    } catch (error) {
+      printError(error.message);
+      throw error
+    }
   }
 }
 
