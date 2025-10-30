dc1fe62e525bd2ed6d3d2b83137c49ba486ca90b
glory-yun <yyk1797@gmail.com>
2025-10-30 16:35:20 +0900

    test(Validate): validateRange 테스트 수정 및 validate.js 모듈 import - validateRange의 배열 입력 방식을 숫자 입력으로 변경했습니다. - validate.js 모듈을 import하였습니다.

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,3 +1,11 @@
+import {
+  validateInputIsEmpty,
+  validateIsInteger,
+  validateNumbersLength,
+  validateNumbersDuplicate,
+  validateRange
+} from "../src/util/validate.js";
+
 describe('Validate 테스트', () => {
   test('로또 구입 금액 문자열이 비어있는 경우 예외가 발생한다.', () => {
     expect(() => {
@@ -30,18 +38,18 @@ describe('Validate 테스트', () => {
 
   test('로또 숫자 범위가 1~45 사이에 있지 않을 경우 예외가 발생한다.', () => {
     expect(() => {
-      const numbers = [0, 1, 2, 3, 4, 5];
-      validateRange(numbers);
+      const input = 0;
+      validateRange(input);
     }).toThrow("[ERROR]")
   })
 
   test('성공적으로 검증에 통과했다.', () => {
     expect(() => {
       validateInputIsEmpty('1,2,3');
-      validateIsNumber('123');
+      validateIsInteger('123');
       validateNumbersLength([1, 2, 3, 4, 5, 6]);
       validateNumbersDuplicate([1, 2, 3, 4, 5, 6]);
-      validateRange([1, 2, 3, 4, 5, 45]);
+      validateRange(1);
     }).not.toThrow();
   });
 })
