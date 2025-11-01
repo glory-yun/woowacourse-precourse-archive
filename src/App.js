b366b55ca625bc169e5efadf274614558ea9b4d6
glory-yun <yyk1797@gmail.com>
2025-11-02 06:35:27 +0900

    feat(App): App 클래스 구현 - run 메서드에서 LottoController를 생성하고 start()를 호출하여 프로그램을 실행하게 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import LottoController from "./controller/LottoController.js";
+
 class App {
-  async run() {}
+  async run() {
+    await new LottoController().start();
+  }
 }
 
 export default App;
+
+
