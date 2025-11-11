c426bf26c049ff82fc8f143053df7f01e1084f35
SUNMI <hariaus@naver.com>
2025-11-11 17:31:48 +0900

    refacotor: submit.html의 form 부분을 <header>,<main>,<footer>만 남겨두고 submit.js에서 생성하도록 분리 - #15

--- frontend/components/submit.js
+++ frontend/components/submit.js
@@ -1,4 +1,42 @@
 const writeFrm = document.querySelector("#writeFrm");
+const header = writeFrm.querySelector("header")
+const main = writeFrm.querySelector("main")
+const footer = writeFrm.querySelector("footer")
+
+window.addEventListener("load", loadForm)
+
+function loadForm() {
+    header.innerHTML = `
+    <div class="mb-3">
+        <label class="form-label">제목</label>
+        <input type="text" class="form-control" placeholder="제목을 입력하세요" id="title" required>
+    </div>
+    `
+
+    main.innerHTML = `
+    <div id="content">
+        <div class="mb-3">
+          <label class="form-label">1️⃣ 이번 주에 배운 점</label>
+          <textarea class="form-control" rows="3" required></textarea>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">2️⃣ 어려웠던 점</label>
+          <textarea class="form-control" rows="3" required></textarea>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">3️⃣ 다음 주 목표</label>
+          <textarea class="form-control" rows="3" required></textarea>
+        </div>
+      </div>
+      `
+
+    footer.innerHTML = `
+    <div class="text-end border-top pt-4">
+        <button type="submit" class="btn btn-primary px-4">저장하기</button>
+      </div>
+    `
+}
+
 writeFrm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
