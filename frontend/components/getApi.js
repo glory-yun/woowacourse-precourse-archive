29bd85bc621bf27502dfc833736da17b24e201c2
SUNMI <hariaus@naver.com>
2025-11-11 22:30:45 +0900

    refactor: 공통 서버 주소를 상수로 분리하여 중 복 제거 - #15

--- frontend/components/getApi.js
+++ frontend/components/getApi.js
@@ -1,20 +1,22 @@
+const url = new URL("http://localhost:8080")
+
 export async function getMemoirs() {
     const response = await
-        fetch("http://localhost:8080");
+        fetch(url);
     const memoirs = await response.json();
     return memoirs;
 }
 
 export async function getMomoirDetail() {
     const response = await
-        fetch("http://localhost:8080");
+        fetch(url);
     const memoir = await response.json();
     return memoir;
 }
 
 export async function postMemoir(memoir) {
     const response = await
-        fetch("http://localhost:8080", {
+        fetch(url, {
             method: "POST",
             body: JSON.stringify({
                 memoir
