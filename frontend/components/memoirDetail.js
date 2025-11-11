9aaaa885a6521d00dbf7d1bb403bddc0f40c3eaa
SUNMI <hariaus@naver.com>
2025-11-11 22:15:47 +0900

    refactor: 예제 데이터 구조 변경에 맞춰 섹션 렌더링 로직 수정 - #15

--- frontend/components/memoirDetail.js
+++ frontend/components/memoirDetail.js
@@ -10,14 +10,22 @@ async function getDetail() {
     const data =
     {
         "title": "11월 회고",
-        "date": "2025-11-08",
+        "date": "2025-11-11",
         "contents": {
-            "section": [
-                { "잘한 점": "코드 리뷰 문화를 정착시켰다." },
-                { "아쉬운 점": "리팩터링 시간을 확보하지 못했다." },
-                { "개선 방안": "리팩터링 전담일을 지정할 것이다." }
+            "sections": [
+                {
+                    "subTitle": "잘한 점",
+                    "description": "계획한 기능들을 모두 완성함"
+                },
+                {
+                    "subTitle": "아쉬운 점",
+                    "description": "테스트 코드 커버리지가 낮았음"
+                },
+                {
+                    "subTitle": "다음주 목표",
+                    "description": "테스트 코드 커버리지가 낮았음"
+                }
             ]
-
         }
     }
     //data = JSON.parse(data)
@@ -30,7 +38,7 @@ async function getDetail() {
 
     title.innerHTML = data.title
     date.innerHTML = data.date
-    learnedThisWeek.innerHTML = data.contents.section[0]["잘한 점"]
-    difficultyThisWeek.innerHTML = data.contents.section[1]["아쉬운 점"]
-    goalNextWeek.innerHTML = data.contents.section[2]["개선 방안"]
+    learnedThisWeek.innerHTML = data.contents.sections[0]["description"]
+    difficultyThisWeek.innerHTML = data.contents.sections[1]["description"]
+    goalNextWeek.innerHTML = data.contents.sections[2]["description"]
 }
