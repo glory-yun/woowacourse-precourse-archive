666bdbd5351d66805d4eae383779000898294d31
glory-yun <yyk1797@gmail.com>
2025-11-14 16:12:57 +0900

    feat: User entity 클래스 구현 - #25 - User entity를 생성하는 클래스를 구현했습니다. - id, username, password 인스턴스를 가질 수 있도록 구현했습니다. - @Getter, @Setter 어노테이션을 사용하여 코드를 단순화 했습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Domain/User.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Domain/User.java
@@ -1,4 +1,25 @@
 package woowatech.open_mission.Domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
 }
