71fb5361a551c4b4787d750cc04f46df07660cf1
glory-yun <yyk1797@gmail.com>
2025-11-14 17:39:44 +0900

    feat: UserService의 회원가입 로직 구현 - #25 - UserContainer에 User를 저장할 수 있는 로직을 구현했습니다. - username 중복 여부를 확인하여 추가했습니다. - 중복 시 IllegalArgumentException 예외를 발생시켜 Controller에서 처리할 수 있도록 했습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Service/UserService.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Service/UserService.java
@@ -1,7 +1,21 @@
 package woowatech.open_mission.Service;
 
 import org.springframework.stereotype.Service;
+import woowatech.open_mission.Domain.User;
+import woowatech.open_mission.Repository.UserContainer;
 
 @Service
 public class UserService {
+    private final UserContainer userContainer;
+
+    public UserService(UserContainer userContainer) {
+        this.userContainer = userContainer;
+    }
+
+    public void register(User user) {
+        if (userContainer.findByUsername(user.getUsername()) != null) {
+            throw new IllegalArgumentException("이미 존재하는 사용자 이름입니다.");
+        }
+        userContainer.save(user);
+    }
 }
