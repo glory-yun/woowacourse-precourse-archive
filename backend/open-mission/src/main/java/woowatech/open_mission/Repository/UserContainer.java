6090559b6e8cd340eee0ea8db16b6b52fd680e21
glory-yun <yyk1797@gmail.com>
2025-11-14 16:11:03 +0900

    feat: userContiner 클래스 구현 - #25 - 회원가입, 로그인을 위하여 User 데이터를 담는 레퍼지토리를 구현했습니다. - user에 따라 회고록을 관리하기 위해 만들었습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Repository/UserContainer.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Repository/UserContainer.java
@@ -1,4 +1,7 @@
 package woowatech.open_mission.Repository;
 
-public class UserContainer {
+import org.springframework.data.jpa.repository.JpaRepository;
+import woowatech.open_mission.Domain.User;
+
+public interface UserContainer extends JpaRepository<User, Long> {
 }
