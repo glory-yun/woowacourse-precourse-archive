84759dfc4ede5eae671a560af5a1d727cf4e3dca
glory-yun <yyk1797@gmail.com>
2025-11-14 22:22:48 +0900

    feat: UserService 로그인 로직 구현 - #25 - 로그인시 LoginRequestDto를 이용해 username, password 검증 로직을 추가했습니다. - 로그인 성공 시 LoginResponseDto(userId, username) 반환하도록 설정했습니다. - 존재하지 않는 사용자명 또는 비밀번호 불일치 시 IllegalArgumentException 발생 처리했습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Service/UserService.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Service/UserService.java
@@ -1,6 +1,8 @@
 package woowatech.open_mission.Service;
 
 import org.springframework.stereotype.Service;
+import woowatech.open_mission.DTO.LoginRequestDto;
+import woowatech.open_mission.DTO.LoginResponseDto;
 import woowatech.open_mission.Domain.User;
 import woowatech.open_mission.Repository.UserContainer;
 
@@ -17,5 +19,20 @@ public class UserService {
             throw new IllegalArgumentException("이미 존재하는 사용자 이름입니다.");
         }
         userContainer.save(user);
+    }
+
+    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
+        String username = loginRequestDto.username();
+        String password = loginRequestDto.password();
+        User user = userContainer.findByUsername(username);
+
+        if(user == null){
+            throw new IllegalArgumentException("존재하지 않는 사용자입니다.");
+        }
+        if(!user.getPassword().equals(password)){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return new LoginResponseDto(user.getUserId(), user.getUsername());
     }
 }
