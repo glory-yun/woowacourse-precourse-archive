9a6b79959bebcc9673b1c3c6ec45386c841321c1
glory-yun <yyk1797@gmail.com>
2025-11-14 22:24:16 +0900

    feat: UserController 로그인 API 구현 - #25 - 로그인 요청 시 LoginRequestDto를 받아 LoginResponseDto 반환하도록 구현했습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Controller/UserController.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import woowatech.open_mission.DTO.LoginRequestDto;
+import woowatech.open_mission.DTO.LoginResponseDto;
 import woowatech.open_mission.Domain.User;
 import woowatech.open_mission.Service.UserService;
 
@@ -29,4 +31,15 @@ public class UserController {
     }
 
     //로그인
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody LoginRequestDto loginRequestDto) {
+        try {
+            LoginResponseDto loginResponseDto = userService.login(loginRequestDto);
+            return ResponseEntity.ok(loginResponseDto);
+        } catch (IllegalArgumentException error) {
+            return ResponseEntity.badRequest().body(error.getMessage()); //400 Bad Request
+        } catch (Exception error) {
+            return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다."); //500 Error
+        }
+    }
 }
