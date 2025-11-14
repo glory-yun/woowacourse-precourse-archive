89203b0083b242f02eedee85e835cd8cfb55eff5
glory-yun <yyk1797@gmail.com>
2025-11-14 16:24:06 +0900

    feat: UserController 기본 구조 생성 - #25 - @RestController 어노테이션으로 REST API 컨트롤러를 생성했습니다. - 회원가입 및 로그인 기능 구현을 위한 주석 위치 지정 - 추후 UserService 연결 예정입니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Controller/UserController.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Controller/UserController.java
@@ -1,4 +1,10 @@
 package woowatech.open_mission.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    //회원 가입
+
+    //로그인
 }
