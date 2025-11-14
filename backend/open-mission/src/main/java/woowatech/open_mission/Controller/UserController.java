b9404176164a7e08917206a8b06dc3ca61b3e49b
glory-yun <yyk1797@gmail.com>
2025-11-14 17:41:14 +0900

    feat: UserController에 회원가입 API 구현 - #25 - /signup에서 클라이언트로부터 User 객체를 @RequestBody로 받아왔습니다. - UserService.register(user)를 호출해 회원가입 로직을 실행하도록 했습니다. - 정상 처리 시 "회원가입이 완료되었습니다." 메시지를 반환하게 구현했습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Controller/UserController.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Controller/UserController.java
@@ -1,10 +1,32 @@
 package woowatech.open_mission.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import woowatech.open_mission.Domain.User;
+import woowatech.open_mission.Service.UserService;
 
 @RestController
 public class UserController {
-    //회원 가입
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody User user) {
+        try {
+            userService.register(user);
+            return ResponseEntity.ok("회원가입이 완료되었습니다.");  //200 OK
+        } catch (IllegalArgumentException error) {
+            return ResponseEntity.badRequest().body(error.getMessage()); //400 Bad Request
+        } catch (Exception error) {
+            return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다."); //500 Error
+        }
+    }
 
     //로그인
 }
