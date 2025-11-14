50ef24793f4fcefc6698e1decc6d2a21d0ea1324
glory-yun <yyk1797@gmail.com>
2025-11-14 17:00:27 +0900

    feat: UserContainer에 username 기반 조회 메서드 추가 - #25 - 회원가입 시 중복 확인 및 로그인 시 사용자 인증 로직에 활용하기 위하여 추가했습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Repository/UserContainer.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Repository/UserContainer.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import woowatech.open_mission.Domain.User;
 
 public interface UserContainer extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
