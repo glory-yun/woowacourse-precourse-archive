f7bdc3c283a3ef6b7ca6aa3c4702f3fdc843c821
SUNMI <hariaus@naver.com>
2025-11-15 17:56:26 +0900

    feat: 로그인 실패 시 예외처리 추가 - #27

--- frontend/components/getUserApi.js
+++ frontend/components/getUserApi.js
@@ -12,14 +12,23 @@ export async function saveUser(user) {
 }
 
 export async function getUser(user) {
-    const response = await
-        fetch(url + "signin", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user)
-        })
-    const user = await response.json();
-    return user;
+    try {
+        const response = await
+            fetch(url + "signin", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user)
+            })
+
+        if (!response.ok) {
+            throw new Error()
+        }
+        user = await response.json();
+        return user;
+    } catch (error) {
+        alert("아이디와 비밀번호를 확인해주세요")
+        throw error
+    }
 }
