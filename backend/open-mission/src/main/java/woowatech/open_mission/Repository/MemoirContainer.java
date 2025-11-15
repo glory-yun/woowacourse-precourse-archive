0ec4e9be31673f88726f88391ed7c1224473f681
glory-yun <yyk1797@gmail.com>
2025-11-15 17:59:21 +0900

    feat: MemoirContainer에 사용자별 회고록 삭제 및 조회 기능 추가 - #25 - @Modifying, @Transactional 어노테이션을 추가하여 커스텀 삭제 쿼리 기능을 구현했습니다. - deleteByIdAndUserId(Long id, Long userId) 메서드로 특정 사용자의 회고록만 삭제할 수 있도록 수정했습니다. - findByIdAndUserId(Long id, Long userId) 메서드로 사용자와 회고록 ID를 함께 조건으로 조회할 수 있게 했습니다.

--- backend/open-mission/src/main/java/woowatech/open_mission/Repository/MemoirContainer.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Repository/MemoirContainer.java
@@ -1,9 +1,15 @@
 package woowatech.open_mission.Repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import woowatech.open_mission.Domain.Memoir;
 
 @Repository
 public interface MemoirContainer extends JpaRepository<Memoir, Long> {
+    @Modifying
+    @Transactional
+    void deleteByIdAndUserId(Long id, Long userId);
+    Memoir findByIdAndUserId(Long id, Long userId);
 }
