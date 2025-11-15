fca58bd6dbd7c27a5b18b00dd91bdc0cc9c9a5be
SUNMI <hariaus@naver.com>
2025-11-15 18:15:56 +0900

    feat: 회원가입 아이디 중복 시 예외처리 추가 - #27

--- frontend/components/getUserApi.js
+++ frontend/components/getUserApi.js
@@ -1,14 +1,22 @@
 const url = new URL("http://localhost:8080/")
 
 export async function saveUser(user) {
-    const response = await
-        fetch(url + "signup", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        })
+    try {
+        const response = await
+            fetch(url + "signup", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user),
+            })
+        if (!response.ok) {
+            throw new Error()
+        }
+    } catch (error) {
+        alert("아이디와 비밀번호를 확인해주세요")
+        throw error
+    }
 }
 
 export async function getUser(user) {
