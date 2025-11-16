c8487117ab3a87919fddf26efc69efa8e3711798
SUNMI <hariaus@naver.com>
2025-11-17 00:46:40 +0900

    feat: User DTO를 Entity로 변환하는 함수 추가 - #40

---
+++ backend/open-mission/src/main/java/woowatech/open_mission/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package woowatech.open_mission.mapper;
+
+import org.springframework.stereotype.Component;
+import woowatech.open_mission.DTO.UserRequestDto;
+import woowatech.open_mission.Domain.User;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserRequestDto userRequestDto) {
+        return User.builder()
+                .username(userRequestDto.username())
+                .password(userRequestDto.password())
+                .email(userRequestDto.email())
+                .build();
+    }
+}
