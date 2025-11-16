ca6fa03df9f7980fca20958a4f0b9bf33ab2070b
SUNMI <hariaus@naver.com>
2025-11-16 22:41:05 +0900

    fix: UserContainer에 @Repository 추가 - #40

--- backend/open-mission/src/main/java/woowatech/open_mission/Repository/UserContainer.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/Repository/UserContainer.java
@@ -2,8 +2,10 @@ package woowatech.open_mission.Repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import woowatech.open_mission.Domain.User;
 
+@Repository
 public interface UserContainer extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
