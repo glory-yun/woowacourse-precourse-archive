807c707f4bb80d7a6a4437643cbd31959b16d87b
glory-yun <yyk1797@gmail.com>
2025-11-17 01:33:50 +0900

    refactor: 회고록 목록 페이지 구조 및 라우팅 로직 개선 - route 구조 분리 및 redirect 추가   - redirectTo(), redirectToWithParam() 함수 생성 - card UI 컴포넌트 분리   - 중복된 URL 생성 코드 제거

--- frontend/JS/memoirList.js
+++ frontend/JS/memoirList.js
@@ -1,49 +1,30 @@
+import { Card } from "../Components/card..js";
 import { getMemoirs } from "../API/getMemoirApi.js";
-import { BASE_URL } from "../config.js";
+import { redirectToMemoirDetail } from "../util/route.js";
 
 const cardContainer = document.querySelector(".cardContainer")
 
-window.addEventListener("load", getList)
+window.addEventListener("load", getMemoirList)
 
-async function getList() {
-    const data = await getMemoirs()
-    makeCard(data)
+async function getMemoirList() {
+  const memoirDtoData = await getMemoirs();
+  makeCard(memoirDtoData);
 }
 
-function makeCard(data) {
-    let memoirList = ""
+function makeCard(memoirDtoData) {
+  const cardsHTML = memoirDtoData.map((data) => Card(data)).join("");
 
-    data.forEach(element => {
+  cardContainer.innerHTML = cardsHTML;
 
-        memoirList += `
-            <div class='card mb-3 p-3 shadow-sm card-hover' id=${element.memoriId}>
-                <h5 class="mb-2">${element.title}</h5>
-                <div class="d-flex justify-content-between align-items-end">
-                <time class="text-muted small" datetime="${element.date}">
-                    ${element.date}
-                </time>
-                <span class="text-muted small">${element.username}</span>
-                </div>
-            </div>
-            `
-    })
-
-    cardContainer.innerHTML = memoirList;
-
-    const card = document.querySelectorAll('.card');
-    card.forEach((element) => {
-        element.addEventListener("click", getDetail)
-    })
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card) => {
+    card.addEventListener("click", getDetail)
+  })
 }
 
 function getDetail(e) {
-    const card = e.target.closest('.card');
-    if (card) {
-        const url = new URL(`${BASE_URL}/memoirDetail.html`)
-        const param = url.searchParams
-
-        param.append("id", card.id)
-
-        window.location.href = url
-    }
+  const card = e.target.closest('.card');
+  if (card) {
+    redirectToMemoirDetail(card.id)
+  }
 }
--- frontend/util/route.js
+++ frontend/util/route.js
@@ -5,10 +5,23 @@ function redirectTo(url, path) {
   window.location.href = `${url}/${path}`;
 }
 
+function redirecToWithParam(id, path) {
+  const url = new URL(`${BASE_URL}/${path}`);
+  url.searchParams.append("id", id);
+  window.location.href = url;
+}
+
+//일반 redirect
 export function redirectToMemoirList() {
   redirectTo(BASE_URL, "memoirList.html");
 }
 
 export function redirectToLogin() {
   redirectTo(LOGIN_URL, "login.html");
+}
+
+
+//params를 사용한 redirect
+export function redirectToMemoirDetail(id) {
+  redirecToWithParam(id, "memoirDetail.html");
 }
