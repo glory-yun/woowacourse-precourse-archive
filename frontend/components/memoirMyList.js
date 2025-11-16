4ea972a34afe0dd8b8a1546a440a6e7f2ef7f7b7
SUNMI <hariaus@naver.com>
2025-11-16 21:16:06 +0900

    feat: 내 회고록 불러오기 로직 추가 - #29
    - html에 js script 추가

---
+++ frontend/components/memoirMyList.js
@@ -0,0 +1,49 @@
+import { getMyMemoirs } from "./getMemoirApi.js";
+// import { memoirList as data } from "./testData.js";
+
+const cardContainer = document.querySelector(".cardContainer")
+
+window.addEventListener("load", getList)
+
+async function getList() {
+    const data = await getMyMemoirs()
+    makeCard(data)
+}
+
+function makeCard(data) {
+    let memoirList = ""
+
+    data.forEach(element => {
+
+        memoirList += `
+            <div class='card mb-3 p-3 shadow-sm card-hover' id=${element.memoriId}>
+                <h5 class="mb-2">${element.title}</h5>
+                <div class="d-flex justify-content-between align-items-end">
+                <time class="text-muted small" datetime="${element.date}">
+                    ${element.date}
+                </time>
+                <span class="text-muted small">${element.username}</span>
+                </div>
+            </div>
+            `
+    })
+
+    cardContainer.innerHTML = memoirList;
+
+    const card = document.querySelectorAll('.card');
+    card.forEach((element) => {
+        element.addEventListener("click", getDetail)
+    })
+}
+
+function getDetail(e) {
+    const card = e.target.closest('.card');
+    if (card) {
+        const url = new URL("http://localhost:5500/frontend/components/memoirDetail.html")
+        const param = url.searchParams
+
+        param.append("id", card.id)
+
+        window.location.href = url
+    }
+}
