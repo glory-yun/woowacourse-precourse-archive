eecd9def45cbf8f93824d16ed39fccd9a0374ae1
SUNMI <hariaus@naver.com>
2025-11-18 14:31:17 +0900

    fix: 변수명 오탈자 수정 - #40

--- backend/open-mission/src/main/java/woowatech/open_mission/memoir/dto/MemoirSummaryDto.java
+++ backend/open-mission/src/main/java/woowatech/open_mission/memoir/dto/MemoirSummaryDto.java
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.util.Date;
 
-public record MemoirSummaryDto(Long memoriId, String title, @JsonFormat(pattern = "yyyy-MM-dd") Date date, String username) {
+public record MemoirSummaryDto(Long memoirId, String title, @JsonFormat(pattern = "yyyy-MM-dd") Date date, String username) {
 }
